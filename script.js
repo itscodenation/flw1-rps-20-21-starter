8e8a471786853e91211f2badaae75e12941fef2c
Peter Jablonski <pwjablonski@gmail.com>
2017-05-14 16:43:58 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+// Under GLOBAL VARIABLES.
+  // TASK 1: 
+    // Declare four global variables. choices, computerChoice, userChoice, and winner.  
+
+// Under DOCUMENT READY FUNCTION.
+  // TASK 2:
+    // Create your document ready function
+  // TASK 3:  
+    // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input docs
+  // TASK 4:
+    // Randomly chooses among 'rock', 'paper', or 'scissors' from the choices array 
+  // TASK 5: 
+    // Compares the user choice and the computer choice to determine who won. 
+  // TASK 6:
+    // Displays the user and computer choices as well as the winner to the output screen
+    // Clears the input box for the next choice
+
+// Under FUNCTIONS
+  // TASK 7:
+    // Refactor your code into functions  
+    // generateComputerChoice takes no parameters and randomly returns "rock", "paper", or "scissor"
+    // pickWinner takes two parameters userChoice and computerChoice and returns the winner 
+    // updateHtml take two parameters, a selector and text, and upadtes the HTML
+
+// *************************** YOUR CODE BELOW *******************************
+//******************TEST EARLY AND OFTEN USING consol.log() ******************
+
+//GLOBAL VARIABLES
+
+
+
+//FUNCTIONS
+
+
+
+// DOCUMENT READY FUNCTION
+
