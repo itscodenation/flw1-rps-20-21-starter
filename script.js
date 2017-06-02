6ee6a4295b98992600fec1ab9c553b70474c11ab
Peter Jablonski <pwjablonski@gmail.com>
2017-06-02 16:12:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-// Under GLOBAL VARIABLES.
-  // TASK 2: 
-    // Declare four global variables for intial state of the list of choices, the computerChoice, the userChoice, and the winner.  
-
-// Under DOCUMENT READY FUNCTION.
-  // TASK 3:
+// TASK 2: 
+  // Under GLOBAL VARIABLES
+    // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
+  // Under DOCUMENT READY FUNCTION
     // Create your document ready function
-    // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input docs
-    // Displays the user choices to the output screen
-  // TASK 4:
-    // Randomly chooses among 'rock', 'paper', or 'scissors' from the choices array 
-    // Displays the user choices to the output screen
-  // TASK 5: 
-    // Compares the user choice and the computer choice to determine who won. 
-    // Displays the user winner to the output screen
-  //TASK 6
-    // Clears the input box for the next choice
+    // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field
+    // Display the user choice to the output screen
+
+// TASK 3:
+    // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
+    // Display the computer choice to the output screen
+
+// TASK 4:
+    // Compare the user choice and the computer choice to determine who won. 
+    // Display the user winner to the output screen
+    // Clear the input box for the next choice
     
-// Under FUNCTIONS
-  // TASK 7:
+// TASK 5:
+  // Under FUNCTIONS
     // Refactor your code into functions  
-    // generateComputerChoice takes no parameters and randomly returns "rock", "paper", or "scissor"
-    // pickWinner takes two parameters userChoice and computerChoice and returns the winner 
-    // updateHtml take two parameters, a selector and text, and upadtes the HTML
+      // generateComputerChoice takes no parameters and randomly returns "rock", "paper", or "scissor"
+      // pickWinner takes two parameters user choice and computer choice and returns the winner 
+      // updateHtml takes two parameters, a selector and text, and updates the HTML
 
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
