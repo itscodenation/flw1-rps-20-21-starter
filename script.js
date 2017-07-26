bf61b6fdc52a9bf10918b2bd0f1ef900f3f2350c
Peter Jablonski <pwjablonski@gmail.com>
2017-07-26 14:22:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-// TASK 2: 
-  // Under GLOBAL VARIABLES
-    // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
-  // Under DOCUMENT READY FUNCTION
-    // Create your document ready function
-    // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field
-    // Display the user choice to the output screen
-
-// TASK 3:
-    // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
-    // Display the computer choice to the output screen
-
-// TASK 4:
-    // Compare the user choice and the computer choice to determine who won. 
-    // Display the user winner to the output screen
-    // Clear the input box for the next choice
-    
-// TASK 5:
-  // Under FUNCTIONS
-    // Refactor your code into functions  
-      // generateComputerChoice takes no parameters and randomly returns "rock", "paper", or "scissor"
-      // pickWinner takes two parameters user choice and computer choice and returns the winner 
-      // updateHtml takes two parameters, a selector and text, and updates the HTML
+//Class 1:
+    //1.1 
+        //Fork and clone the repo and set up your project workspace and link files
+    //1.2 
+        // Write HTML for your project
+//Class 2:  
+    //2.1 
+        // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
+        // Create your document ready function, 
+        // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
+        // Display the user choice to the output screen
+    //2.2 
+        // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
+        // Display the computer choice to the output screen
+//Class 3:  
+    //3.1 
+        // Compare the user choice and the computer choice to determine who won. 
+        // Display the user winner to the output screen 
+        // Clear the input box for the next choice
+    //3.2  
+        // generateComputerChoice takes no parameters and randomly returns "rock", "paper", or "scissor", 
+        // pickWinner takes two parameters user choice and computer choice and returns the winner, 
+        // updateHtml takes two parameters, a selector and text, and updates the HTML
 
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
