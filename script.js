fd326c683ba7cc25dc8b471cb52973a367b22597
Peter Jablonski <pwjablonski@gmail.com>
2017-08-07 12:01:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,20 @@
         // Write HTML for your project
 //Class 2:  
     //2.1 
+        // Practice using console.log() and debugger to debug your code
+    //2.2 
         // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
         // Create your document ready function. 
         // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
         // Display the user choice to the output screen
-    //2.2 
-        // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
-        // Display the computer choice to the output screen
 //Class 3:  
     //3.1 
+        // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
+        // Display the computer choice to the output screen
+    //3.2  
         // Compare the user choice and the computer choice to determine who won. 
         // Display the user winner to the output screen 
         // Clear the input box for the next choice
-    //3.2  
-        // generateComputerChoice takes no parameters and randomly returns "rock", "paper", or "scissor", 
-        // pickWinner takes two parameters user choice and computer choice and returns the winner, 
-        // updateHtml takes two parameters, a selector and text, and updates the HTML
 
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
