553962846854382c7e7769eb75151fca3f36d612
Peter Jablonski <pwjablonski@gmail.com>
2017-12-12 14:14:45 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-//Class 1:
-        // Fork and clone the repo and set up your project workspace and link files
-        // Write HTML for your project
-//Class 2:  
-        // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
-        // Practice using console.log() and debugger to debug your code
-        // Create your document ready function. 
-        // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
-        // Display the user choice to the output screen
-//Class 3:  
-        // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
-        // Display the computer choice to the output screen
-        // Compare the user choice and the computer choice to determine who won. 
-        // Display the user winner to the output screen 
-        // Clear the input box for the next choice
-
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
@@ -22,9 +6,6 @@
 
 
 
-//FUNCTIONS
 
-
-
-// DOCUMENT READY FUNCTION
+// DOCUMENT READY FUNCTION BELOW
 
