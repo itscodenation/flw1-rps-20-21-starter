b13463c77a47dec12f5161c82dbb8b9c3e1faf72
Glitch (enchanted-bouncy-limit) <none>
2021-11-17 23:12:41 +0000

    💺🚝 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+let choice = "";
+let randNum = 0;
 
+$(".play").click(function() {
+  choice = $("input").val();
+  $(".userInput").text(choice);
+  randNum = Math.floor(Math.random() * 3);
+  $(".computerPlay").text(randNum);
+});
