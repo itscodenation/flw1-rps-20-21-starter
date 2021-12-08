4f3db4e9ef055bb6056e274a67b6a43cab9d2e0c
Glitch (rock-paper-scissors-proje) <none>
2021-12-08 23:27:38 +0000

    👯🎱 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,78 +1,91 @@
 let computerChoiceNum = ["rock", "paper", "scissors"];
+let totalWins = 0;
+let totalTies = 0;
+let totalLoses = 0;
+let totalGames = 0;
+let winRate = totalWins / totalGames;
 
-let randomNumber = 0;
-
-let result;
-// User clicks button
 
 // Thunpitcha's function
 // Docs: https://docs.google.com/document/d/1gq7oUif59sKDInbJrDTIeSQBaOlGb5ri06b2-noWWNI/edit#heading=h.x4lnrrw34ze2
 function getRandomComputerChoice() {
-   return computerChoiceNum;
-  randomNumber = Math.floor(Math.random() * 3);
+  let randomNumber = Math.floor(Math.random() * 3);
 
-  $(".computerChoice").text(computerChoiceNum[randomNumber]);
+  let choice;
 
- 
+  if (randomNumber === 0) {
+    choice = "rock";
+  } else if (randomNumber === 1) {
+    choice = "scissors";
+  } else {
+    choice = "paper";
+  }
+
+  return choice; // value = "rock", "paper", "scissors"
 }
+
+$(".clear").click(function() {
+  $(".input").val("");
+  $(".userChoice").text("");
+  $(".result").text("");
+  $(".computerChoice").text("");
+});
+
 $(".play").click(function() {
+
+  let result;
+  totalGames = totalGames + 1;
+
+  let choices = getRandomComputerChoice();
+
   let user_choice = $(".input").val();
 
   $(".userChoice").text(user_choice);
 
   user_choice = user_choice.toLowerCase();
 
-  randomNumber = Math.floor(Math.random() * 3);
+  $(".computerChoice").text(choices);
 
-  $(".computerChoice").text(computerChoiceNum[randomNumber]);
-
-  if (user_choice == "rock" && computerChoiceNum[randomNumber] == "scissors") {
+  if (user_choice == "rock" && choices == "scissors") {
     result = "You Won!";
-  } else if (
-    user_choice == "rock" &&
-    computerChoiceNum[randomNumber] == "rock"
-  ) {
+  } else if (user_choice == "rock" && choices == "rock") {
     result = "You Tied!";
-  } else if (
-    user_choice == "rock" &&
-    computerChoiceNum[randomNumber] == "paper"
-  ) {
+  } else if (user_choice == "rock" && choices == "paper") {
     result = "You Lost!";
-  } else if (
-    user_choice == "paper" &&
-    computerChoiceNum[randomNumber] == "rock"
-  ) {
+  } else if (user_choice == "paper" && choices == "rock") {
     result = "You Won!";
-  } else if (
-    user_choice == "paper" &&
-    computerChoiceNum[randomNumber] == "scissors"
-  ) {
+  } else if (user_choice == "paper" && choices == "scissors") {
     result = "You Lost!";
-  } else if (
-    user_choice == "paper" &&
-    computerChoiceNum[randomNumber] == "paper"
-  ) {
+  } else if (user_choice == "paper" && choices == "paper") {
     result = "You Tied!";
-  } else if (
-    user_choice == "scissors" &&
-    computerChoiceNum[randomNumber] == "scissors"
-  ) {
+  } else if (user_choice == "scissors" && choices == "scissors") {
     result = "You Tied!";
-  } else if (
-    user_choice == "scissors" &&
-    computerChoiceNum[randomNumber] == "rock"
-  ) {
+  } else if (user_choice == "scissors" && choices == "rock") {
     result = "You Lost!";
-  } else if (
-    user_choice == "scissors" &&
-    computerChoiceNum[randomNumber] == "paper"
-  ) {
+  } else if (user_choice == "scissors" && choices == "paper") {
     result = "You Won!";
   } else {
     $(".result").append(`<p>Your input is invalid, try again.<p>`);
   }
-
-  // use 'result'
+  
+  for (totalWins = 0; totalWins <= totalGames;){
+      totalWins = totalWins +1;
+  }
+  for (totalLoses = 0; totalLoses <= totalGames; ){
+    totalLoses = totalLoses +1;
+  }
+  for (totalTies = 0; totalTies <= totalGames; ){
+    totalTies = totalTies +1;
+//     move the writing out then just change the variable
+}
+  
+  
+    // use 'result'
+    $(".ties").text("You have tied" + totalTies + "times");
+    $(".loses").text("You have lost" + totalLoses + "times");
+      $(".wins").text("You have won" + totalWins + "times");
 
   $(".result").text(result);
+  $(".winRate").text("Your win rate is" + winRate + "percent");
+  $(".games").text("You have played" + totalGames + "times");
 });
